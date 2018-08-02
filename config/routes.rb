886ceaa2f49ee47be5d1b5eb105b011902852051
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-02 15:12:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   
-  scope '/admin', module: 'admin' do
+  namespace :admin do
     resources :stats, only: [:index]
-  end
+end
 
   root 'posts#index'
 end
